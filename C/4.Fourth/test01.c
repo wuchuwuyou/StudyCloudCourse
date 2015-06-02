d51df4adf7236c0b57957c57b476088c56579b0f
WangTeng <woaijia_xxx@126.com>
2015-06-02 10:37:02 +0800

    c path 4 test01

---
+++ C/4.Fourth/test01.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+	//两数之间 素数和
+	int m,n;
+	scanf("%d %d",&m,&n);
+	int max = m>n?m:n;
+	int min = m>n?n:m;
+	int sum = 0;
+	for (int i = min; i <= max; i++)
+	{
+		//素数
+		int isOk = 1;
+		for (int j = 2; j < i; j++)
+		{
+			if ( i % j == 0)
+			{
+				isOk = 0;
+			}
+		}
+		if (isOk)
+		{
+			sum += i;
+			// printf("%d\n", i);
+		}
+	}
+
+	printf("sum = %d\n",sum);
+
+	return 0;
+}
