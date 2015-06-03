6ee11da9c1c3fa6b339d268c6fd5235d87c6398f
WangTeng <woaijia_xxx@126.com>
2015-06-03 11:40:01 +0800

    c path 5th test05

---
+++ C/5.Fifth/test05.c
@@ -0,0 +1,77 @@
+#include "stdio.h"
+
+void readability(int i);
+void strength(int i); 
+
+int main(int argc, char const *argv[])
+{
+	int m;
+	scanf("%d",&m);
+	if(m > 59 || m <11){
+		printf("请输入正确的信号数值[11,59]\n");
+		return -1;
+	}
+
+	strength(m%10);
+	printf(",");
+	readability(m/10);
+	printf(".\n");
+
+	return 0;
+}
+void readability(int i){
+
+	switch(i){
+		case 1:
+			printf("unreadable");
+			break;
+		case 2:
+			printf("barely readable,occasional words distinguishable");
+			break;
+		case 3:
+			printf("readable with considerable diffculty");
+			break;
+		case 4:
+			printf("readable with practically no difficulty");
+			break;
+		case 5:
+			printf("perfectly readable");
+			break;
+		default:
+			break;
+
+	}
+}
+void strength(int i){
+	switch(i){
+		case 1:
+			printf("Faint signals,barely perceptible");
+			break;
+		case 2:
+			printf("Very weak signals");
+			break;
+		case 3:
+			printf("Weak signals");
+			break;
+		case 4:
+			printf("Fair signals");
+			break;
+		case 5:
+			printf("Fairly good signals");
+			break;
+		case 6:
+			printf("Good signals");
+			break;
+		case 7:
+			printf("Moderately strong signals");
+			break;
+		case 8:
+			printf("Strong signals");
+			break;
+		case 9:
+			printf("Extremely strong signals");
+			break;
+		default:
+			break;	
+	}
+}
