bb20d6e439a990511e59dcf527eb28e89dcb8067
WangTeng <woaijia_xxx@126.com>
2015-06-04 11:13:08 +0800

    fixed bug

--- C/6.Sixth/test02.c
+++ C/6.Sixth/test02.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+/*
 //这个有问题  如果同行同列中数据相同则无法处理
 int main(int argc, char const *argv[])
 {
@@ -61,4 +62,55 @@ int main(int argc, char const *argv[])
 		}
 	}
 	return 0;
-}
+}*/
+//改进后
+ int main(int argc, char const *argv[])
+ {
+ 	int matrix[100][100] = {0};
+ 	int m ;
+ 	scanf("%d",&m);
+ 	for (int i = 0; i < m ; ++i)
+ 	{
+ 		for (int j = 0; j < m; ++j)
+ 		{
+ 			scanf("%d",&matrix[i][j]);
+ 		}
+ 	}
+
+ 	int count = 0,judge = 1; // judge 判断条件用
+
+ 	for (int i = 0; i < m; ++i)  // 行
+ 	{
+ 		for (int j = 0; j < m; ++j) //列
+ 		{
+ 			judge = 1;
+ 			//判断行上最大
+ 			for (int x = 0; x < m; ++x)
+ 			{
+ 				if (matrix[i][j] < matrix[i][x])
+ 				{
+ 					judge = 0;
+ 				}
+ 			}	
+ 			if(judge == 0) continue;
+ 			//判断列上最小
+ 			for (int y = 0; y < m; ++y)
+ 			{
+ 				if (matrix[i][j] > matrix[y][j])
+ 				{
+ 					judge = 0;
+ 				}
+ 			}
+ 			if (judge == 0) continue;
+ 			count ++;
+            printf("第%d个鞍点：%d，位于第%d行、第%d列\n",count,matrix[i][j],i,j);
+
+ 		}
+ 		
+ 	}
+ 	if (count == 0)
+ 		{
+ 			printf("NO\n");
+ 		}
+ 	return 0;
+ }
